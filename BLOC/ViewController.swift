c33725b159b40eb112e58490fd408922f5f34a52
Berkat Bhatti <beme@theknightmarket.com>
2018-01-07 17:40:44 -0500

    Initial Commit

---
+++ BLOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLOC
+//
+//  Created by berkat bhatti on 1/7/18.
+//  Copyright © 2018 TKM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
